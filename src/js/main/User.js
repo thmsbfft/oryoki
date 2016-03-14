1def139fcd4b8c47a732b244f6a7ca0e98a20468
thmsbfft <thmsbfft@gmail.com>
2016-03-14 15:00:40 -0700

    Move app data to Application Support

--- src/js/main/User.js
+++ src/js/main/User.js
@@ -4,7 +4,18 @@ function User(name) {
 
 	// Storing in ~/ for now
 	// Will be moved to user's choice
-	this.confPath = app.getPath('home');
+	this.confPath = app.getPath('appData');
+
+	// Check if Oryoki has data
+	fs.access(this.confPath + '/Oryoki/', fs.F_OK, (err) => {
+		if(err) {
+			c.log('No access!');
+			fs.mkdir(this.confPath + '/Oryoki/', 0777, (err) => {
+				if (err.code == 'EEXIST') cb(null);
+				else c.log(err);
+			});
+		}
+	});
 
 	this.preferences = undefined;
 	this.bookmarks = undefined;
@@ -28,4 +39,13 @@ User.prototype.watchFile = function(fileName, callback) {
 
 User.prototype.getConfFile = function(fileName) {
 	return JSON.parse(fs.readFileSync(this.confPath + '/Oryoki/' + fileName, 'utf8'));
+	// fs.access(this.confPath + '/Oryoki/' + fileName, fs.F_OK, (err) => {
+	// 	if(err) {
+	// 		c.log('Error accessing configuration file: ', err);
+	// 		fs.writeFile(this.confPath + '/Oryoki/' + fileName, fs.readFileSync(__dirname + '/src/data/factory.json', 'utf8'), (err) => {
+	// 			if (err) c.log(err);
+	// 		});
+	// 		return JSON.parse(fs.readFileSync(this.confPath + '/Oryoki/' + fileName, 'utf8'));
+	// 	}
+	// });
 }
