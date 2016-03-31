ef3c21c2da6478ab9014779293ab746f4e39526a
thmsbfft <thmsbfft@gmail.com>
2016-03-30 20:53:43 -0700

    Lay out features for WindowHelper

--- src/js/renderer/WindowHelper.js
+++ src/js/renderer/WindowHelper.js
@@ -48,42 +48,114 @@ WindowHelper.prototype.attachEvents = function() {
 		this.select();
 	});
 
+	this.el.addEventListener('keyup', function(e) {
+		this.onInputKeyUp(e);
+	}.bind(this));
+
 }
 
 WindowHelper.prototype.updateWindowDimensions = function() {
 
-	var newWidth = document.querySelectorAll('#frame')[0].offsetWidth;
-	var newHeight = document.querySelectorAll('#frame')[0].offsetHeight;
-	console.log(newHeight);
+	this.widthInput.value = document.querySelectorAll('#frame')[0].offsetWidth;
+	this.heightInput.value = document.querySelectorAll('#frame')[0].offsetHeight;
 
-	if(newWidth >= 1000) {
+	this.updateUI();
+
+}
+
+WindowHelper.prototype.updateUI = function() {
+
+	if(this.widthInput.value >= 1000) {
 		addClass(this.el.querySelectorAll('#width')[0], 'fourDigits');
 	}
 	else {
 		removeClass(this.el.querySelectorAll('#width')[0], 'fourDigits');		
 	}
 
-	this.el.querySelectorAll('#width')[0].value = newWidth;
-
-	if(newHeight >= 1000) {
+	if(this.heightInput.value >= 1000) {
 		addClass(this.el.querySelectorAll('#height')[0], 'fourDigits');
 	}
 	else {
 		removeClass(this.el.querySelectorAll('#height')[0], 'fourDigits');		
 	}	
 
-	this.el.querySelectorAll('#height')[0].value = newHeight;
-
 }
 
 WindowHelper.prototype.hide = function() {
-	console.log('Hiding');
+
 	this.isVisible = false;
 	this.el.className = 'hide';
+
 }
 
 WindowHelper.prototype.show = function() {
-	console.log('Showing');
+
 	this.isVisible = true;
 	this.el.className = 'show';
+
+}
+
+WindowHelper.prototype.onInputKeyUp = function(e) {
+
+	console.log(e);
+
+	switch(e.keyCode) {
+
+		case 38:
+			// Arrow Up
+			break;
+
+		case 40:
+			// Arrow Down
+			break;
+
+		case 9:
+			// Tab
+			break;
+
+		case 13:
+			// Enter Key
+			this.requestNewWindowDimensions(
+				this.widthInput.value, this.heightInput.value
+			);
+			break;
+
+	}
+
+	this.updateUI();
+
+}
+
+WindowHelper.prototype.requestNewWindowDimensions = function(width, height) {
+
+	console.log('Resizing to: ' + width + 'x' + height);
+
+}
+
+WindowHelper.prototype.slider = function(e, direction) {
+
+	// TODO need a new name
+
+	switch(direction) {
+
+		case 'up':
+			if(e.shiftKey) {
+				// Increment by 10
+			}
+			else {
+				// Increment by 1
+			}
+			break;
+
+		case 'down':
+			if(e.shiftKey) {
+				// Decrement by 10
+			}
+			else {
+				// Decrement by 1
+			}
+			break;
+
+	}
+
 }
