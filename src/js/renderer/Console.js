c34ff390b894fb9f01f93613b6a95345a93d4c8b
thmsbfft <thmsbfft@gmail.com>
2016-05-19 16:23:25 -0700

    Add basic js execution to mini console

--- src/js/renderer/Console.js
+++ src/js/renderer/Console.js
@@ -1,13 +1,39 @@
 function Console(parameters) {
 
 	this.el = document.getElementsByTagName('console')[0];
+	this.input = this.el.getElementsByTagName('input')[0];
+
+	this.attachEvents();
 	this.hide();
-	
+
+}
+
+Console.prototype.attachEvents = function() {
+
+	this.input.addEventListener('focus', this.clear.bind(this));
+	this.input.addEventListener('keyup', function(e) {
+		if (e.keyCode == 13) { this.submit(); }
+		else if(e.key == "Escape") { this.hide(); }
+	}.bind(this));
+
 }
 
 Console.prototype.updateMessage = function(e) {
 
-	this.el.innerHTML = e.message;
+	this.input.value = e.message;
+
+}
+
+Console.prototype.clear = function() {
+
+	this.input.value = '';
+
+}
+
+Console.prototype.submit = function() {
+
+	Browser.view.webview.executeJavaScript(this.input.value);
+	this.clear();
 
 }
 
@@ -20,5 +46,6 @@ Console.prototype.hide = function() {
 Console.prototype.show = function() {
 
 	this.el.className = 'show';
+	this.input.focus();
 
 }
