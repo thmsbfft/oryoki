8af958f68dd5aa570b31593b098a38e95399f5b6
thmsbfft <thmsbfft@gmail.com>
2017-08-16 15:28:02 -0700

    refine build process

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const path = require('path')
+const webpack = require('webpack')
+const isProd = process.env.NODE_ENV === 'production'
+
+console.log('Production:', isProd)
 
 module.exports = {
   entry: {
@@ -13,6 +17,14 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+          options: { presets: ['es2015'] }
+        }]
+      },
+      {
         test: /\.scss$/,
         use: [
           { loader: 'style-loader' },
@@ -22,5 +34,16 @@ module.exports = {
       }
     ]
   },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      comments: !isProd,
+      mangle: isProd,
+      sourceMap: !isProd,
+      compress: {
+        warnings: false,
+        drop_console: isProd
+      }
+    })
+  ],
   target: 'electron'
 }
